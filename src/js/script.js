7d244d227a8150cfdb60bc502d0ccb0ccfcfd859
JuanMOLopes <juan.oliiveira2007@gmail.com>
2024-11-03 21:03:40 -0300

    js inicial

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,11 @@
-let conteudo = document.getElementById("conteudo");
+let renascimento = document.getElementById("inovacoes-renascentistas");
+let atualidade = document.getElementById("inovacoes-atuais");
 
 function exibirRenascimento() {
-  conteudo.innerText("teste");
+  atualidade.style.display = "none";
+  renascimento.style.display = "block";
+}
+function exibirAtualidade() {
+  renascimento.style.display = "none";
+  atualidade.style.display = "block";
 }
