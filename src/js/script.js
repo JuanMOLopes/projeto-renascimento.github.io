5374102e6bb26189503eac5592fecf96ea4f9366
JuanMOLopes <juan.oliiveira2007@gmail.com>
2024-11-04 00:48:42 -0300

    imagens, js e css

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,34 @@ function exibirAtualidade() {
   renascimento.style.display = "none";
   atualidade.style.display = "block";
 }
+
+let sidebar = document.getElementById("sidebar");
+let botao = document.getElementById("toggleButton");
+
+function exibirSidebar() {
+  if (sidebar.classList.contains("expanded")) {
+    sidebar.classList.remove("expanded");
+    sidebar.style.width = "0";
+    botao.style.left = "0";
+    botao.textContent = ">";
+  } else {
+    sidebar.classList.add("expanded");
+    sidebar.style.width = "250px";
+    botao.style.left = "250px";
+    botao.textContent = "<";
+  }
+}
+
+function exibirSidebarBarra() {
+  sidebar.classList.add("expanded");
+  sidebar.style.width = "250px";
+  botao.style.left = "250px";
+  botao.textContent = "<";
+}
+
+function removerSidebar() {
+  sidebar.classList.remove("expanded");
+  sidebar.style.width = "0";
+  botao.style.left = "0";
+  botao.textContent = ">";
+}
